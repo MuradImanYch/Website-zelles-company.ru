cc5b0dce2c9f7fdda7232e40d1549d4f7d6c95c3
Murad Imanov <mura.imanov@gmail.com>
2022-03-12 18:05:32 +0400

    project is done 11

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 let barToggle = false;
+let btn_show = $("#btn_show");
 
 $(document).ready(() => {
     $('.barsWrap').click(() => { // menu bar anim func
         barToggle = !barToggle;
         if(barToggle) {
-            $('header .barsWrap .bar3').css({width: '0px'}).css({background: '#fff'});
+            $('header .barsWrap .bar3').css({display: 'none'});
             $('header .barsWrap .bar1').css({transform: 'translate(0, 10px) rotate(45deg)'}).css({width: '15px'}).css({background: '#131313'});
             $('header .barsWrap .bar2').css({transform: 'rotate(-45deg)'}).css({width: '15px'}).css({background: '#131313'});
             $('.header .mobMenuWrap').fadeIn();
@@ -12,8 +13,9 @@ $(document).ready(() => {
         else {
             $('header .barsWrap .bar1').css({transform: 'translate(0, 0) rotate(0deg)'}).css({width: '35px'}).css({background: '#fff'});
             $('header .barsWrap .bar2').css({transform: 'rotate(0deg)'}).css({width: '35px'}).css({background: '#fff'});
-            $('header .barsWrap .bar3').css({transform: 'rotate(0deg)'}).css({width: '35px'}).css({background: '#fff'});
+            $('header .barsWrap .bar3').fadeIn(700).css({display: 'block'}).css({transform: 'rotate(-180deg)'}).css({width: '35px'}).css({background: '#fff'});
             $('.header .mobMenuWrap').fadeOut();
         }
     });
+
 });
