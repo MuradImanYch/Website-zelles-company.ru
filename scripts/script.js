bbee8939eb1e0686e1384dfe646bbe345ffc599e
Murad Imanov <mura.imanov@gmail.com>
2022-03-12 13:56:57 +0400

    project is done 10

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ $(document).ready(() => {
     $('.barsWrap').click(() => { // menu bar anim func
         barToggle = !barToggle;
         if(barToggle) {
+            $('header .barsWrap .bar3').css({width: '0px'}).css({background: '#fff'});
             $('header .barsWrap .bar1').css({transform: 'translate(0, 10px) rotate(45deg)'}).css({width: '15px'}).css({background: '#131313'});
             $('header .barsWrap .bar2').css({transform: 'rotate(-45deg)'}).css({width: '15px'}).css({background: '#131313'});
             $('.header .mobMenuWrap').fadeIn();
@@ -11,6 +12,7 @@ $(document).ready(() => {
         else {
             $('header .barsWrap .bar1').css({transform: 'translate(0, 0) rotate(0deg)'}).css({width: '35px'}).css({background: '#fff'});
             $('header .barsWrap .bar2').css({transform: 'rotate(0deg)'}).css({width: '35px'}).css({background: '#fff'});
+            $('header .barsWrap .bar3').css({transform: 'rotate(0deg)'}).css({width: '35px'}).css({background: '#fff'});
             $('.header .mobMenuWrap').fadeOut();
         }
     });
