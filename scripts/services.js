8aca8935a56c22b6e18107ccd9edee7678c44cb5
Murad Imanov <mura.imanov@gmail.com>
2022-03-14 21:38:12 +0400

    project is done 16

--- scripts/services.js
+++ scripts/services.js
@@ -5,21 +5,14 @@ $(document).ready(() => {
     $('.filter button:nth-child(1)').click(() => {
         $('.filter button:nth-child(1)').css({background: '#272339'}).css({color: '#fff'});
         $('.filter button:not(.filter button:nth-child(1))').css({background: '#000'}).css({color: 'rgba(255, 255, 255, 0.4)'});
-        $('.grapDes, .promotion').hide();
+        $('.grapDes').hide();
         $('.webDev').fadeIn();
     });
     $('.filter button:nth-child(2)').click(() => {
         $('.filter button:nth-child(2)').css({background: '#272339'}).css({color: '#fff'});
         $('.filter button:not(.filter button:nth-child(2))').css({background: '#000'}).css({color: 'rgba(255, 255, 255, 0.4)'});
-        $('.webDev, .promotion').hide();
+        $('.webDev').hide();
         $('.grapDes').fadeIn();
         $('.grapDes').css({display: 'flex'});
-    });
-    $('.filter button:nth-child(3)').click(() => {
-        $('.filter button:nth-child(3)').css({background: '#272339'}).css({color: '#fff'});
-        $('.filter button:not(.filter button:nth-child(3))').css({background: '#000'}).css({color: 'rgba(255, 255, 255, 0.4)'});
-        $('.webDev, .grapDes').hide();
-        $('.promotion').fadeIn();
-        $('.promotion').css({display: 'flex'});
     });
 });
